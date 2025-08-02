1549366eef05dce555d5244faf47a714be4773f0
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-08-02 18:17:24 +0700

    Update TestTaskMapApplication.java

--- src/main/java/org/itk/testtaskmap/TestTaskMapApplication.java
+++ src/main/java/org/itk/testtaskmap/TestTaskMapApplication.java
@@ -8,6 +8,14 @@ public class TestTaskMapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestTaskMapApplication.class, args);
+
+        String[] words = {"день", "ночь", "день", "утро", "вечер", "ночь"};
+        Map<String, Long> wordCount = countElements(words);
+        System.out.println("Подсчет строк: " + wordCount);
+
+        int[] primitiveNumbers = {1, 2, 3, 2, 1, 3, 1, 4};
+        Map<Integer, Long> primitiveCount = countElements(primitiveNumbers);
+        System.out.println("Подсчет интов: " + primitiveCount);
     }
 
 }
