35bb1f617638dd5d3438089c6236a76c252e0097
Canib-us <162473942+Canib-us@users.noreply.github.com>
2025-08-02 18:16:48 +0700

    Update ArrayToMapUtil.java

--- src/main/java/org/itk/testtaskmap/ArrayToMapUtil.java
+++ src/main/java/org/itk/testtaskmap/ArrayToMapUtil.java
@@ -1,4 +1,25 @@
 package org.itk.testtaskmap;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ArrayToMapUtil {
+    public static <T> Map<T, Long> countElements(T[] array) {
+        return Arrays.stream(array)
+                .collect(Collectors.groupingBy(
+                        element -> element,
+                        Collectors.counting()
+                ));
+    }
+
+    //перегруженный метод для интов, для остальных примитивов необходимо сделать аналогичные
+    public static Map<Integer, Long> countElements(int[] array) {
+        return Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        element -> element,
+                        Collectors.counting()
+                ));
+    }
 }
